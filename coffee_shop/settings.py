3413e7c94a43b1a42f173991ab7aa050a1fcb0a2
smjt2000 <smjavadt.1379@gmail.com>
2025-01-29 12:53:58 +0330

    unistalled django-htmx and removed it from settings.py

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'coffee_shop.urls'
